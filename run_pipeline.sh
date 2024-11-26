0e2b87a3bc5546873b87561e2a56207dfaba5058
Michael Hess <Michaelhess17@yahoo.com>
2024-11-26 19:24:39 +0000

    begin pipeline script

---
+++ run_pipeline.sh
@@ -0,0 +1,6 @@
+# Train the network
+/mnt/Mouse_Face_Project/Desktop/Data/Python/NeuralODE/.pixi/envs/default/bin/python train_network.00.py
+
+# Generate data from the model
+/mnt/Mouse_Face_Project/Desktop/Data/Python/NeuralODE/.pixi/envs/default/bin/python generate_data_from_model.py
+
