327ba452ae3aea58acf77d870fa5c578cdddef18
Michael Hess <Michaelhess17@yahoo.com>
2024-11-26 19:25:54 +0000

    cleaned train_network file

--- train_network.00.py
+++ train_network.00.py
@@ -14,7 +14,6 @@ from scipy.signal import savgol_filter
 import matplotlib.pyplot as plt
 
 window_length = 30
-polyorder = 3
 data = jnp.load("outputs/VDP_oscillators.npy")[:, :, ::3]
 data = data.reshape(data.shape[0]*data.shape[1], data.shape[2], data.shape[3])
 
@@ -31,14 +30,6 @@ convolve_trials = vmap(convolve_features, in_axes=0, out_axes=0)
 # Apply convolution to all trials and features at once
 new_data = convolve_trials(data)
 
-# feats = data.shape[-1]
-# n_trials = data.shape[0]
-# new_data = jnp.zeros((n_trials, data.shape[1], data.shape[2]))
-# for trial in range(n_trials):
-#     # savgol filter
-#     # new_data = new_data.at[trial, :, :feats].set(savgol_filter(data[trial, :, :], window_length=window_length, polyorder=polyorder, axis=0))
-#     # or moving average
-#     new_data = new_data.at[trial, :, :feats].set(jnp.convolve(data[trial, :, :], jnp.ones((window_length,))/window_length, mode='valid'))
 
 # Standardize the data
 new_data = (new_data - jnp.mean(new_data, axis=1)[:, None, :]) / jnp.std(new_data, axis=1)[:, None, :]
@@ -55,6 +46,7 @@ skip = 300
 _, k, τ = embed_data(np.array(data[:, skip:, :1]))
 
 data_tde = takens_embedding(data[:, :, :1], τ, k)
+
 print("Embedded data shape: ", data_tde.shape)
 
 
@@ -66,15 +58,15 @@ ts, ys, model = jax_utils.train_NODE(
     t1=5.0,
     width_size=128,
     hidden_size=256,
-    ode_size=8,
-    depth=3,
-    batch_size=128,
-    seed=69,
-    lr_strategy=(8e-4,),
-    steps_strategy=(50000,),
-    length_strategy=(1,),
-    skip_strategy=(2,),
-    seeding_strategy=(1/5,),
+    ode_size=6,
+    depth=2,
+    batch_size=256,
+    seed=6969,
+    lr_strategy=(8e-4, 3e-4),
+    steps_strategy=(40000, 70000),
+    length_strategy=(0.3, 1.0),
+    skip_strategy=(1, 2),
+    seeding_strategy=(0.1, 0.25),
     plot=False,
     print_every=1000,
     k=1,
